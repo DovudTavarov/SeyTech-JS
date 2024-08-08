0985d7364f62d1060b1c0d8e0188d058fa68ad05
DovudTavarov <dovudtavarov54@gmail.com>
2024-08-07 21:19:17 -0400

    added more functions

--- main.js
+++ main.js
@@ -29,3 +29,51 @@ let weatherTodayByCel = fahrenToCel(Math.floor(Math.random() * 100));
 console.log(
   `It is ${weatherTodayByFahren}°F today. That's ${weatherTodayByCel}°C.`
 );
+
+/* The Fortune Teller: With Functions! */
+
+function tellFortune(numOfChild, partName, geoLocation, jobTitle) {
+  return `You will be a ${jobTitle} in ${geoLocation}, and married to ${partName} with ${numOfChild} kids.`;
+}
+
+console.log(tellFortune(2, "Olha", "Philly", "Developer"));
+console.log(tellFortune(3, "Anna", "New York", "Draver"));
+console.log(tellFortune(1, "Lana", "San Jose", "Actor"));
+
+/* What number is bigger? */
+
+function whichNumLarger(num1, num2) {
+  if (num1 > num2) {
+    return `Number ${num1} is larger`;
+  } else if (num1 < num2) {
+    return `Number ${num2} is larger`;
+  } else if (num1 === num2) {
+    return "Error";
+  }
+}
+
+console.log(whichNumLarger(20, 10));
+console.log(whichNumLarger(15, 30));
+console.log(whichNumLarger(50, 50));
+
+/* Pluralize */
+
+function pluralize(num, word) {
+  if (num === 1) {
+    return `${num} ${word}`;
+  } else {
+    return `${num} ${word}s`;
+  }
+}
+
+console.log(pluralize(1, "cat"));
+console.log(pluralize(3, "sheep"));
+console.log(pluralize(10, "geese"));
+
+/* Even/Odd Counter */
+
+for (let num = 0; num < 20; num++) {
+  if (num % 2 === 0) {
+    console.log(`${num} is even`);
+  }
+}
